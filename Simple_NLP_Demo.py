c1694ef7c467d438c1df7c05e5a70340a9793a94
Paul DADA <60022063+Geo-Paull@users.noreply.github.com>
2021-06-11 23:07:04 +0100

    Update Simple_NLP_Demo.py

--- Simple_NLP_Demo.py
+++ Simple_NLP_Demo.py
@@ -47,7 +47,7 @@ st.sidebar.text('(c) 2021')
 #Textbox for text user is entering
 st.text("Type/paste the text you'd like to analyze.")
 # text = st.text_input('Enter text', 'Enter Text Here')
-st.success('Almost there, paste text & run SERVICE.')
+st.info('Almost there, paste text & run SERVICE.')
 text = st.text_area('===>>>', value='Type/Paste Text Here...', height=100, max_chars=None, help='Text must be more than a Sentence long.')
 
 st.button("RUN Service")
